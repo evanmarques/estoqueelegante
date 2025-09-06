8310c8a3dd502d308c1d06e86a97ba29d229631f
e.marques <evanprogti@gmail.com>
2025-09-06 14:39:53 -0300

    att

---
+++ backend/src/main/java/br/com/estoqueelegante/api/produto/Produto.java
@@ -0,0 +1,28 @@
+package br.com.estoqueelegante.api.produto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private double price;
+    private int quantitystock;
+    private long barcode;
+}
---
+++ backend/src/main/java/br/com/estoqueelegante/api/produto/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package br.com.estoqueelegante.api.produto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+}
