3250ed5330c6c530e47edcc357c1e165e67676e9
e.marques <evanprogti@gmail.com>
2025-09-08 13:47:16 -0300

    Agora sumiu os icones de paginas e esta tudo preto.

--- frontend/app/src/app/admin/menu/menu.routes.ts
+++ frontend/app/src/app/admin/menu/menu.routes.ts
@@ -1,28 +1,21 @@
 import { Routes } from '@angular/router';
-import { MenuPage } from './menu.page';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: MenuPage,
-    children: [
-      {
-        path: 'produtos',
-        loadComponent: () => import('../pages/produto-lista/produto-lista.page').then( m => m.ProdutoListaPage)
-      },
-      {
-        path: 'produtos/novo',
-        loadComponent: () => import('../pages/produto-cadastro/produto-cadastro.page').then( m => m.ProdutoCadastroPage)
-      },
-      {
-        path: 'produtos/editar/:id',
-        loadComponent: () => import('../pages/produto-editar/produto-editar.page').then( m => m.ProdutoEditarPage)
-      },
-      {
-        path: '',
-        redirectTo: 'produtos',
-        pathMatch: 'full'
-      }
-    ]
-  }
+    {
+      path: 'produtos',
+      loadComponent: () => import('../pages/produto-lista/produto-lista.page').then( m => m.ProdutoListaPage)
+    },
+    {
+      path: 'produtos/novo',
+      loadComponent: () => import('../pages/produto-cadastro/produto-cadastro.page').then( m => m.ProdutoCadastroPage)
+    },
+    {
+       path: 'produtos/editar/:id',
+       loadComponent: () => import('../pages/produto-editar/produto-editar.page').then( m => m.ProdutoEditarPage)
+    },
+    {
+      path: '',
+      redirectTo: 'produtos', // Se o usuário digitar apenas /admin, será levado para /admin/produtos
+      pathMatch: 'full'
+    }
 ];
--- frontend/app/src/app/app-routing.module.ts
+++ frontend/app/src/app/app-routing.module.ts
@@ -3,15 +3,22 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '', // Rota raiz continua sendo a vitrine (Tabs)
+    path: '',
+    redirectTo: 'tabs', // Redireciona a rota raiz para a vitrine
+    pathMatch: 'full'
+  },
+  {
+    path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   // CORREÇÃO APLICADA AQUI
   {
     path: 'admin',
-    // Agora carregamos o ARQUIVO DE ROTAS do menu, não o componente diretamente.
+    // Carrega o COMPONENTE do Menu...
+    loadComponent: () => import('./admin/menu/menu.page').then( m => m.MenuPage),
+    // ...e TAMBÉM carrega as ROTAS FILHAS que serão exibidas dentro do Menu.
     loadChildren: () => import('./admin/menu/menu.routes').then( m => m.routes)
-  },
+  }
 ];
 @NgModule({
   imports: [
