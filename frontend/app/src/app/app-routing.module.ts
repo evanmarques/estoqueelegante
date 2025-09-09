2a9365d98a40d6e04a8c91eea6333eef106e142d
e.marques <evanprogti@gmail.com>
2025-09-09 11:14:45 -0300

    Agora esta ok o acesso, porém apresentando as três tabs apenas, sem apresentar os menus que foram criados para papel admin

--- frontend/app/src/app/app-routing.module.ts
+++ frontend/app/src/app/app-routing.module.ts
@@ -4,14 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'tabs', // Redireciona a rota raiz para a vitrine
-    pathMatch: 'full'
-  },
-  {
-    path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
-  // CORREÇÃO APLICADA AQUI
   {
     path: 'admin',
     // Carrega o COMPONENTE do Menu...
@@ -19,7 +13,7 @@ const routes: Routes = [
     // ...e TAMBÉM carrega as ROTAS FILHAS que serão exibidas dentro do Menu.
     loadChildren: () => import('./admin/menu/menu.routes').then( m => m.routes)
   }
-];
+]; 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
