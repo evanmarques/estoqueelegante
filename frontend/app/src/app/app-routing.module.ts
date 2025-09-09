032268fae20c2fe7b67e3e100b8473233b8bbbdd
e.marques <evanprogti@gmail.com>
2025-09-09 11:52:27 -0300

    Fazendo voltas nos erros apenas.

--- frontend/app/src/app/app-routing.module.ts
+++ frontend/app/src/app/app-routing.module.ts
@@ -1,19 +1,20 @@
+// ARQUIVO: frontend/app/src/app/app-routing.module.ts
+
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
     path: '',
+    // Carrega o MÓDULO de rotas das abas
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
     path: 'admin',
-    // Carrega o COMPONENTE do Menu...
-    loadComponent: () => import('./admin/menu/menu.page').then( m => m.MenuPage),
-    // ...e TAMBÉM carrega as ROTAS FILHAS que serão exibidas dentro do Menu.
+    // Carrega as ROTAS do menu admin (que é standalone)
     loadChildren: () => import('./admin/menu/menu.routes').then( m => m.routes)
   }
-]; 
+];
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
--- frontend/app/src/app/app.module.ts
+++ frontend/app/src/app/app.module.ts
@@ -1,19 +1,23 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HttpClientModule } from '@angular/common/http'; // 1. Adicione esta linha
-
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule], // 2. Adicione esta linha aqui
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-
 export class AppModule {}
