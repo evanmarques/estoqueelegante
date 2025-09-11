f005f1ec0f08f9923be0921b6339480832138e6e
e.marques <evanprogti@gmail.com>
2025-09-11 13:48:51 -0300

    Atualizações no backend, pom

--- backend/src/main/resources/db/migration/V2__add-image-url-to-products.sql
+++ backend/src/main/resources/db/migration/V2__add-image-url-to-products.sql
@@ -1,1 +1,5 @@
-ALTER TABLE products ADD COLUMN image_url VARCHAR(255);
+-- V2__add-image-url-to-products.sql
+-- Adiciona a coluna image_url na tabela products para armazenar a URL da imagem do produto
+
+ALTER TABLE products
+ADD COLUMN image_url VARCHAR(255);
